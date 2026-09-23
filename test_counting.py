37700a764a74f1014c2907502e12b66e971bd223
vit-shourya-here <shourya2024vit@gmail.com>
2026-09-23 21:55:42 +0530

    Fixed len error

--- test_counting.py
+++ test_counting.py
@@ -1,10 +1,10 @@
 import pytest
 from counting import find_max, count_even
 
-@pytest.mark.parametrize("numbers", "expected", [([1,2,3], 3), ([9,12,21], 21)])
+@pytest.mark.parametrize("numbers, expected", [([1,2,3], 3), ([9,12,21], 21)])
 def test_find_max(numbers, expected):
     assert find_max(numbers) == expected
 
-@pytest.mark.parametrize("num", "exp", [([2,3,4], 2), ([2,4,6], 3)])
+@pytest.mark.parametrize("num, exp", [([2,3,4], 2), ([2,4,6], 3)])
 def test_count_even(num, exp):
     assert count_even(num) == exp
